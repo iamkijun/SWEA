1247903e2c059e3a85303ae62e8a069ecd6f4250
leeguain@gmail.com <leeguain@gmail.com>
2022-07-26 17:56:58 +0900

    7/26

---
+++ D2/1954.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for i in range(1,T+1):
+    
+    N = int(input())
+    li =[]
+    li_small = [0] *N 
+    li = [li_small] * N
+    
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            if j == 1:
+                li[i][j] = j
+            elif j >
+            
+        if i > N:
+            li.append(li.small)
+            li_small.clear()
+
+    print(li)
+    
+    
---
+++ del.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+while N // 10 > 0:
+    print(N)
+    N = N // 10
